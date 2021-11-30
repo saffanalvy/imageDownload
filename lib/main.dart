8fd162efe18e0bad6674d7853742e74ab5b69dcb
saffanalvy <saffanalvy@gmail.com>
2021-11-30 08:58:22 +0600

    Syncronizes with the bucket and displays the images in a list

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fileupload/api/firebase_api.dart';
+import 'package:fileupload/model/firebase_file.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<FirebaseFile>> futureFiles;
+
+  @override
+  void initState() {
+    super.initState();
+
+    futureFiles = FirebaseApi.listAll('files/');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder<List<FirebaseFile>>(
+          future: futureFiles,
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const Center(child: CircularProgressIndicator());
+              default:
+                if (snapshot.hasError) {
+                  return const Center(child: Text('Some error occured!'));
+                } else {
+                  final files = snapshot.data!;
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        buildHeader(files.length),
+                        const SizedBox(height: 12),
+                        Expanded(
+                            child: ListView.builder(
+                                itemCount: files.length,
+                                itemBuilder: (context, index) {
+                                  final file = files[index];
+                                  return buildFile(context, file);
+                                })),
+                      ],
+                    ),
+                  );
+                }
+            }
+          },
+        ));
+  }
+
+  Widget buildFile(BuildContext context, FirebaseFile file) {
+    return ListTile(
+      leading: Image.network(
+        file.url,
+        width: 52,
+        height: 52,
+        fit: BoxFit.cover,
+      ),
+      title: Text(
+        file.name,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.underline,
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  Widget buildHeader(int i) {
+    var blue = Colors.blue;
+    return ListTile(
+      tileColor: blue,
+      leading: const Icon(Icons.file_copy),
+      title: Text(i.toString() + ' files'),
+    );
+  }
+}
